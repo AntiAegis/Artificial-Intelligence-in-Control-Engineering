401ef07e1f28d4844ceb6671b7313c41a46b7369
AntiAegis <thuypn9a4@gmail.com>
2018-10-02 20:44:21 +0700

    Full code of Particle Filter

--- ex2-particle-filter/utils/process_model.py
+++ ex2-particle-filter/utils/process_model.py
@@ -1,12 +1,14 @@
 #------------------------------------------------------------------------------
 #	Libraries
 #------------------------------------------------------------------------------
+import numpy as np
+np.random.seed(0)
 
 
 #------------------------------------------------------------------------------
 #	Function represents the process model
 #------------------------------------------------------------------------------
-def process_model(x,u, sigma_v, sigma_g , wb, time_step):
+def process_model(x, u, sigma_v, sigma_g, wb, time_step):
 	"""
 	[Arguments]
 		x : (ndarray) Current position, including x coordinate, y coordinate,
@@ -19,36 +21,43 @@ def process_model(x,u, sigma_v, sigma_g , wb, time_step):
 
 		sigma_g : (float) Standard deviation of the stearing angle noise.
 
+		wb : (float) 
+
+		time_step : (float) The interval between consecutive controls.
+
 	[Return]
 		x_pred : (ndarray) Predicted position, including x coordinate, y
 			coordinate and angle (radian). The shape of x_pred is (3, 1).
 	"""
 	# calculate V* = V + delta(V)
-    new_V = u[0] + sigma_v * np.random.randn()
-    
-    # calculate G* = G + delta(G)
-    new_G = u[1] + sigma_g * np.random.randn()
-    
-    # calculate new angle 
-    new_angle = x[2] + new_V * time_step * np.sin(new_G)/ wb
-    
-    # calculate y
-    new_y = x[1]+ new_V * time_step * np.sin(new_G + new_angle)
-    
-    # calculate x
-    new_x = x[0] + new_V * time_step * np.cos(new_G + new_angle)
-    
-    # result
-    new_coordinates=np.array([[new_x],[new_y],[new_angle]])
-    return new_coordinates
+	new_V = u[0, 0] + sigma_v*np.random.randn()
 
+	# calculate G* = G + delta(G)
+	new_G = u[1, 0] + sigma_g*np.random.randn()
 
+	# calculate new angle 
+	new_angle = x[2, 0] + new_V * time_step * np.sin(new_G) / wb
+
+	# calculate y
+	new_y = x[1, 0]+ new_V * time_step * np.sin(new_G + new_angle)
+
+	# calculate x
+	new_x = x[0, 0] + new_V * time_step * np.cos(new_G + new_angle)
+
+	# result
+	x_pred = np.array([[new_x], [new_y], [new_angle]])
+	return x_pred
 
 
 #------------------------------------------------------------------------------
 #	Test bench to verify the written function
 #------------------------------------------------------------------------------
-x= np.array([1,1,1]) # input array 
-u= [2,3] # control signal
-a= process_model(x,u, 0.5,0.5,4,1) 
-print(a) # result
+# x = np.ones([3, 1], dtype=float)
+# u = np.array([[2], [3]], dtype=float)
+# sigma_v, sigma_g = 0.5, 0.5
+# wb = 4.0
+# time_step = 0.05
+
+# a = process_model(x, u, sigma_v, sigma_g, wb, time_step) 
+# print(a)
+# print(a.shape)
